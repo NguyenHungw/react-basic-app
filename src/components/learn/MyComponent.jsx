3dd38073ee06765ab0467cb631ed5045acd48287
NguyenHungw <hwajio.o@gmail.com>
2024-07-20 09:40:47 +0700

    vid30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,11 @@
-
+import './style.css'
 const Mycomponent = () => {
     return (
-      <div>
-        react component
-      </div>
+        <>
+         <div> react component</div>
+         <div className="child" style={{borderRadius:"10px"}}>Test</div>
+        </>
+     
     );
   }
   export default Mycomponent
