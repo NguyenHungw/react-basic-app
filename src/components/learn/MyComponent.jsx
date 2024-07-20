ae353d6ed22faa24d54a1eea0957bb5dc8cbca54
NguyenHungw <hwajio.o@gmail.com>
2024-07-20 10:11:52 +0700

    vid31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,22 @@
 import './style.css'
 const Mycomponent = () => {
+
+    // const test = "string"
+    // const test = 12
+    const test = [1,2,3]
+    // const test = Boolean
+    // const test = undefined
+    // const test = null
+
+    // const test = {
+    //     name:"hung",
+    //     age:21
+    // }
+
     return (
         <>
-         <div> react component</div>
+         <div style={{color:"red"}} > {JSON.stringify(test)} component</div>
+         <div>{console.log("Eric")}</div>
          <div className="child" style={{borderRadius:"10px"}}>Test</div>
         </>
      
