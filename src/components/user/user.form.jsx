1d3957d59e0b7b39da94ca4fd5c88f8925acd7aa
NguyenHungw <hwajio.o@gmail.com>
2024-07-28 17:32:36 +0700

    vid67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,29 +1,52 @@
-import { Input } from "antd"
+import { Button, Input } from "antd"
+import { useState } from "react"
 
 const UserForm = () => {
+    const [fullName,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [phone,setPhone] = useState("");
+
+
+    const HandleClickBtn = () => {
+        console.log("check full form",{fullName,email,password,phone})  
+      }
+    
     return (
 
-        <div className="user-form" >
-            <div>
+        <div className="user-form" style={{margin:"20px 0"}}  >
+            <div style={{display:"flex",gap:"15px",flexDirection:"column"}}>
                 <div>
                     <span>FullName</span>
-                    <Input/>
+                    <Input 
+                        value={fullName}
+                        onChange={(event)=>{setName(event.target.value)}}
+                    />
+                    
                 </div>
                 <div>
-                    <span>FullName</span>
-                    <Input/>
+                    <span>Email</span>
+                    <Input
+                        value={email}
+                        onChange={(event)=>{setEmail(event.target.value)}}
+                    />
                 </div>
                 <div>
-                    <span>FullName</span>
-                    <Input/>
+                    <span>Password</span>
+                    <Input.Password
+                        value={password}
+                        onChange={(event)=> {setPassword(event.target.value)}}
+                    />
                 </div>
                 <div>
-                    <span>FullName</span>
-                    <Input/>
+                    <span>PhoneNumber</span>
+                    <Input
+                        value={phone}
+                        onChange={(event)=>{setPhone(event.target.value)}}
+                    />
                 </div>
                 <div>
-                    <span>FullName</span>
-                    <Input/>
+                    <Button type="primary" onClick={HandleClickBtn} > Create User </Button>
                 </div>
             </div>
         </div>
