6ac071a438baa955a8c128c8c21e96d089f94e4e
NguyenHungw <hwajio.o@gmail.com>
2024-08-13 17:37:06 +0700

    13/8

--- src/components/book/view.book.detail.jsx
+++ src/components/book/view.book.detail.jsx
@@ -16,7 +16,16 @@ const BookDetail = (props) => {
         {dataViewDetail ? (
           <>
         <p>{dataViewDetail._id}</p>
-        <p>{dataViewDetail.thumbnail}</p>
+        <div style={{
+              marginTop: "10px",
+              height: "100px", width: "150px",
+              border: "1px solid #ccc"
+            }}>
+              <img style={{ height: "100%", width: "100%", objectFit: "contain" }}
+
+                src={`${import.meta.env.VITE_BACKEND_URL}/images/book/${dataViewDetail.thumbnail}`}
+              ></img>
+            </div>
         <p>{dataViewDetail.mainText}</p>
         <p>{dataViewDetail.author}</p>
         <p>{dataViewDetail.price}</p>
